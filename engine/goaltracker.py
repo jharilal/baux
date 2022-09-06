66b9428fecf91464c58b1cc1d7d2bd2f90787968
jharilal <jordanharilal@gmail.com>
2022-09-06 18:55:04 -0400

    Added html files, and images; created backend skeleton

---
+++ engine/goaltracker.py
@@ -0,0 +1,20 @@
+"""Uses other files to track goals"""
+
+
+class Goaltracker:
+    """Will be the main backbone to the webapp"""
+    
+    def add_goal():
+        pass
+
+    def del_goal():
+        pass
+
+    def confirm_activity():
+        pass
+
+    def create_date():
+        pass
+
+    def check_streak():
+        pass
